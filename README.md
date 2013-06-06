45a0aff3707640288a11d50b7a415ade4da1dfb1
ndahfi <phiquadrat@gmail.com>
2013-06-06 18:28:28 +0700

    Add button for guessing letter. Not implemented.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 funcodingtugas2
 ===============
+
+ndah to zakazai:
+Gue pake Windowbuildernya Eclipse, kalo mau pake itu juga buat class baru dari
+Swing Designer trus copas kode yang udah ada.
--- lala/src/gui/GuiTugas2.java
+++ lala/src/gui/GuiTugas2.java
@@ -6,6 +6,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLayeredPane;
+import java.awt.FlowLayout;
+import java.awt.CardLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.Font;
 
 public class GuiTugas2 extends JFrame {
 
@@ -31,12 +46,56 @@ public class GuiTugas2 extends JFrame {
 	 * Create the frame.
 	 */
 	public GuiTugas2() {
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(GuiTugas2.class.getResource("/res/10.png")));
+		setTitle("FunCoding Tugas 2 - Loop");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 586, 356);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JMenuBar menuBar = new JMenuBar();
+		contentPane.add(menuBar, BorderLayout.NORTH);
+		
+		JMenu game = new JMenu("Game");
+		menuBar.add(game);
+		
+		JMenuItem newGame = new JMenuItem("New");
+		game.add(newGame);
+		
+		JMenuItem giveUp = new JMenuItem("See Asnwer");
+		game.add(giveUp);
+		
+		JMenu quit = new JMenu("Quit");
+		menuBar.add(quit);
+		
+		JLayeredPane layeredPane = new JLayeredPane();
+		contentPane.add(layeredPane);
+		layeredPane.setLayout(null);
+		
+		JLabel hangman = new JLabel("");
+		hangman.setVerticalAlignment(SwingConstants.TOP);
+		hangman.setIcon(new ImageIcon(GuiTugas2.class.getResource("/res/10.png")));
+		hangman.setBounds(10, 11, 270, 281);
+		layeredPane.add(hangman);
+		
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEADING);
+		panel.setBounds(289, 11, 271, 279);
+		layeredPane.add(panel);
+		
+		JButton[] alphabet = new JButton[26]; 
+		for(int i = 'A'; i <= 'Z'; i++){
+			char button = (char) i;
+			alphabet[i-'A'] = new JButton(""+button);
+			alphabet[i-'A'].setFont(new Font("Consolas", Font.PLAIN, 14));
+			panel.add(alphabet[i-'A']);
+		}
+		JButton tes = new JButton("tes");
+		tes.setFont(new Font("Consolas", Font.PLAIN, 11));
+		panel.add(tes);
 	}
-
 }
