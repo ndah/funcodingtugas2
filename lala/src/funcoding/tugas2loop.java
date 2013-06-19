6289d24f64c6e961553fdd03f81d8176767d8f38
Endah <phiquadrat@gmail.com>
2013-06-19 13:23:33 +0700

    Update versi selesai

--- lala/src/funcoding/tugas2loop.java
+++ lala/src/funcoding/tugas2loop.java
@@ -1,30 +1,175 @@
 package funcoding;
 
+import gui.GuiTugas2;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
 
-public class Tugas2Loop {
-   public Tugas2Loop() {
-	   
-   }
-   
-   public String getTombol() {
-	   return null;
-   }
-   
-   public boolean isSelesai(){
-	   return false;
-   }
-   
-   public void resetGame() {
-	   
-   }
-   
-   public void startNewGame() {
-	   
-   }
-   
-   public void endGame(){
-	   
-   }
-   
-   
+/**
+ * 
+ * @author Nur Endah Safitri
+ * 
+ */
+public class Tugas2loop implements ActionListener {
+	private String[] countries;
+	private boolean[] rightGuesses;
+	private static GuiTugas2 frame;
+	private int chances;
+	private boolean win;
+	private String guessThisString;
+
+	/**
+	 * Baca String dari file 'Countries.txt' di folder res untuk ke array of
+	 * String <code>countries</code>.
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	private void loadStrings() {
+		Scanner in = new Scanner(
+				Tugas2loop.class.getResourceAsStream("/res/countries.txt"));
+		countries = new String[199];
+		// /////////////////////////////////////////////////////////////////
+		// Soal 1
+		// For loop untuk membaca input.
+		for (int i = 0; i < countries.length; i++) {
+			countries[i] = in.nextLine();
+		}
+		// Soal 1 end
+		// /////////////////////////////////////////////////////////////////
+	}
+
+	/**
+	 * Menampilkan jawaban jika user menjawab benar, mengurangi kesempatan untuk
+	 * menebak jika user salah.
+	 * 
+	 * @param guessThis
+	 *            String yang akan dtebak
+	 * @param character
+	 *            String satu huruf yang dimasukkan
+	 */
+	public void guess(String character) {
+		boolean right = false;
+		// /////////////////////////////////////////////////////////////////
+		// Soal 2
+		// For loop untuk menebak.
+		for (int i = 0; i < guessThisString.length(); i++) {
+			if (character.equalsIgnoreCase("" + guessThisString.charAt(i))) {
+				frame.guessThis[i].setVisible(true);
+				rightGuesses[i] = true;
+				right = true;
+			}
+		}
+		// Soal 2 end
+		// /////////////////////////////////////////////////////////////////
+
+		// /////////////////////////////////////////////////////////////////
+		// Soal 3
+		// For loop untuk cek apakah sudah menang atau belum.
+		win = true;
+		for (int i = 0; i < guessThisString.length(); i++) {
+			if (!rightGuesses[i]) {
+				win = false;
+			}
+		}
+		if (win) {
+			frame.setMessage("Correct guess!");
+			frame.playAgain.setVisible(true);
+		}
+		// Soal 3 end
+		// /////////////////////////////////////////////////////////////////
+
+		if (!right) {
+			--chances;
+			frame.changePicture(chances);
+			if (chances <= 0) {
+				seeAnswer();
+				frame.setMessage("Oops, the right answer is: ");
+				frame.playAgain.setVisible(true);
+			}
+		}
+	}
+
+	/**
+	 * Set semua guessThis jadi visible.
+	 */
+	public void seeAnswer() {
+		chances = 0;
+		// /////////////////////////////////////////////////////////////////
+		// Soal 4
+		// For loop untuk melihat jawaban.
+		for (int i = 0; i < guessThisString.length(); i++) {
+			frame.guessThis[i].setVisible(true);
+		}
+		frame.playAgain.setVisible(true);
+		// Soal 4 end
+		// /////////////////////////////////////////////////////////////////
+	}
+
+	/**
+	 * Ambil String secara random dari array <code>countries</code>.
+	 * 
+	 * @return String random dari array <code>countries</code>
+	 */
+	public String getRandomString() {
+		loadStrings();
+		Random r = new Random();
+		int random = r.nextInt(199);
+		guessThisString = countries[random];
+		rightGuesses = new boolean[guessThisString.length()];
+		return guessThisString;
+	}
+
+	/**
+	 * Constructor
+	 */
+	public Tugas2loop() {
+		chances = 10;
+	}
+
+	/**
+	 * Listener untuk semua kejadian.
+	 */
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if (source instanceof JMenuItem) {
+			JMenuItem menu = (JMenuItem) source;
+			String command = menu.getText();
+			if (command.equals("New")) {
+				frame.dispose();
+				frame = new GuiTugas2();
+				frame.setVisible(true);
+			} else if (command.equals("See Answer") && !win) {
+				seeAnswer();
+				frame.setMessage("Why give up? >_<.");
+			} else if (command.equals("Quit")) {
+				System.exit(0);
+			}
+		}
+		if (source instanceof JButton) {
+			JButton tombol = (JButton) source;
+			String command = tombol.getText();
+			if (command.equals("Yes")) {
+				frame.dispose();
+				frame = new GuiTugas2();
+				frame.setVisible(true);
+			} else if (command.equals("No")) {
+				frame.playAgain.setVisible(false);
+			} else if (chances > 0 && !win) {
+				frame.alphabet[tombol.getText().toUpperCase().charAt(0) - 'A']
+						.setEnabled(false);
+				guess(tombol.getText());
+			}
+		}
+	}
+
+	/**
+	 * Run the application.
+	 * 
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		frame = new GuiTugas2();
+		frame.setVisible(true);
+	}
 }
--- lala/src/gui/GuiTugas2.java
+++ lala/src/gui/GuiTugas2.java
@@ -1,121 +1,195 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import funcoding.Tugas2Loop;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import java.awt.Toolkit;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.JLayeredPane;
-import java.awt.FlowLayout;
-import java.awt.CardLayout;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JMenu;
-import javax.swing.SwingConstants;
-import javax.swing.JButton;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import funcoding.Tugas2loop;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class GuiTugas2 extends JFrame {
 
+	public Tugas2loop t;
+	public JLabel guessThis[];
+	public JButton[] alphabet;
+	public JPanel playAgain;
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String [] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GuiTugas2 frame = new GuiTugas2();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JLabel messageLabel;
+	private JLabel hangman;
 
 	/**
 	 * Create the frame.
 	 */
 	public GuiTugas2() {
+		t = new Tugas2loop();
 		setResizable(false);
-		setIconImage(Toolkit.getDefaultToolkit().getImage(GuiTugas2.class.getResource("/res/11.png")));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(
+				GuiTugas2.class.getResource("/res/10.png")));
 		setTitle("FunCoding Tugas 2 - Loop");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 586, 500);
 		contentPane = new JPanel();
-		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		// contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
-		
 		JMenuBar menuBar = new JMenuBar();
 		contentPane.add(menuBar, BorderLayout.NORTH);
-		
+
 		JMenu game = new JMenu("Game");
 		menuBar.add(game);
-		
+
 		JMenuItem newGame = new JMenuItem("New");
+		newGame.addActionListener(t);
 		game.add(newGame);
-		
-		JMenuItem giveUp = new JMenuItem("See Asnwer");
+
+		JMenuItem giveUp = new JMenuItem("See Answer");
+		giveUp.addActionListener(t);
 		game.add(giveUp);
-		
-		JMenu quit = new JMenu("Quit");
-		menuBar.add(quit);
-		
-		JLayeredPane layeredPane = new JLayeredPane();
-		contentPane.add(layeredPane);
-		layeredPane.setLayout(null);
-		
-		final JLabel hangman = new JLabel("");
-		hangman.setVerticalAlignment(SwingConstants.TOP);
-		hangman.setIcon(new ImageIcon(GuiTugas2.class.getResource("/res/10.png")));
-		hangman.setBounds(10, 11, 270, 281);
-		layeredPane.add(hangman);
-		
+
+		JMenuItem quit = new JMenuItem("Quit");
+		quit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		game.add(quit);
+
+		JLayeredPane mainPanel = new JLayeredPane();
+		contentPane.add(mainPanel);
+		GridBagLayout gbl_mainPanel = new GridBagLayout();
+		gbl_mainPanel.columnWidths = new int[] { 280, 261, 0 };
+		gbl_mainPanel.rowHeights = new int[] { 291, 150, 0 };
+		gbl_mainPanel.columnWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
+		gbl_mainPanel.rowWeights = new double[] { 0.0, 0.0, 1.0 };
+		mainPanel.setLayout(gbl_mainPanel);
+
+		JPanel hangmanPanel = new JPanel();
+		GridBagConstraints gbc_hangmanPanel = new GridBagConstraints();
+		gbc_hangmanPanel.insets = new Insets(0, 0, 0, 5);
+		gbc_hangmanPanel.fill = GridBagConstraints.BOTH;
+		gbc_hangmanPanel.gridx = 0;
+		gbc_hangmanPanel.gridy = 0;
+		mainPanel.add(hangmanPanel, gbc_hangmanPanel);
+
+		hangman = new JLabel("");
+		hangman.setHorizontalAlignment(SwingConstants.CENTER);
+		hangman.setFont(new Font("Kristen ITC", Font.PLAIN, 22));
+		hangmanPanel.add(hangman);
+		hangman.setText("Hangman");
+
 		JPanel panel = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
-		flowLayout.setAlignment(FlowLayout.LEADING);
-		final JLabel labelHuruf = new JLabel();
-		labelHuruf.setBounds(100, 100,1000,1000);
-		panel.setBounds(289, 11, 271, 279);
-		layeredPane.add(panel);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 1;
+		gbc_panel.gridy = 0;
+		mainPanel.add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] { 261, 0 };
+		gbl_panel.rowHeights = new int[] {50, 30, 50, 30, 0};
+		gbl_panel.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0 };
+		panel.setLayout(gbl_panel);
+
+		JPanel message = new JPanel();
+		FlowLayout flowLayout_1 = (FlowLayout) message.getLayout();
+		GridBagConstraints gbc_message = new GridBagConstraints();
+		gbc_message.fill = GridBagConstraints.HORIZONTAL;
+		gbc_message.anchor = GridBagConstraints.NORTH;
+		gbc_message.insets = new Insets(0, 0, 5, 0);
+		gbc_message.gridx = 0;
+		gbc_message.gridy = 0;
+		panel.add(message, gbc_message);
+
+		messageLabel = new JLabel("Guess the country name!");
+		messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		messageLabel.setFont(new Font("Segoe UI Light", Font.PLAIN, 24));
+		message.add(messageLabel);
+
+		JPanel guess = new JPanel();
+		guess.setLayout(null);
+		GridBagConstraints gbc_guess = new GridBagConstraints();
+		gbc_guess.fill = GridBagConstraints.BOTH;
+		gbc_guess.anchor = GridBagConstraints.NORTH;
+		gbc_guess.insets = new Insets(0, 0, 5, 0);
+		gbc_guess.gridx = 0;
+		gbc_guess.gridy = 2;
+		panel.add(guess, gbc_guess);
 		
+		JPanel pointer = new JPanel();
+		pointer.setLayout(null);
+		GridBagConstraints gbc_pointer = new GridBagConstraints();
+		gbc_pointer.insets = new Insets(0, 0, 5, 0);
+		gbc_pointer.fill = GridBagConstraints.BOTH;
+		gbc_pointer.anchor = GridBagConstraints.NORTH;
+		gbc_pointer.gridx = 0;
+		gbc_pointer.gridy = 3;
+		panel.add(pointer, gbc_pointer);
 		
+		playAgain = new JPanel();
+		GridBagConstraints gbc_playAgain = new GridBagConstraints();
+		gbc_playAgain.fill = GridBagConstraints.BOTH;
+		gbc_playAgain.gridx = 0;
+		gbc_playAgain.gridy = 4;
+		panel.add(playAgain, gbc_playAgain);
 		
-		JButton[] alphabet = new JButton[26]; 
-		for(int i = 'A'; i <= 'Z'; i++){
-			char button = (char) i;
-			alphabet[i-'A'] = new JButton(""+button);
-			alphabet[i-'A'].setFont(new Font("Consolas", Font.PLAIN, 14));
-			final int huruf = i;
-			alphabet[i-'A'].addActionListener(new ActionListener(){
-				public void actionPerformed(ActionEvent e)
-	            {
-	                System.out.println("You clicked the "+ (char) huruf +" button");
-	                labelHuruf.setFont(new Font("Consolas",Font.PLAIN, 30));
-	                labelHuruf.setText(labelHuruf.getText() + (char) huruf+" ");
-	                String isian = "/res/"+((huruf%10)+1)+".png";
-	                hangman.setIcon(new ImageIcon(GuiTugas2.class.getResource(isian)));
-	            }
-			});
-			panel.add(alphabet[i-'A']);
+		JLabel playAgainLabel = new JLabel("Play Again?");
+		playAgainLabel.setFont(new Font("Segoe UI Light", Font.PLAIN, 24));
+		playAgain.add(playAgainLabel);
+		playAgain.setVisible(false);
+		
+		JButton yesPlay = new JButton("Yes");
+		playAgain.add(yesPlay);
+		yesPlay.addActionListener(t);
+		
+		JButton noPlay = new JButton("No");
+		playAgain.add(noPlay);
+		noPlay.addActionListener(t);
+
+		JPanel button = new JPanel();
+		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.fill = GridBagConstraints.BOTH;
+		gbc_button.insets = new Insets(0, 0, 5, 0);
+		gbc_button.gridwidth = 2;
+		gbc_button.gridy = 1;
+		gbc_button.gridx = 0;
+		mainPanel.add(button, gbc_button);
+
+		String guessThisString = t.getRandomString();
+		guessThis = new JLabel[guessThisString.length()];
+		for (int i = 0; i < guessThisString.length(); i++) {
+			guessThis[i] = new JLabel("" + guessThisString.charAt(i));
+			guess.add(guessThis[i]);
+			guessThis[i].setForeground(Color.GRAY);
+			guessThis[i].setFont(new Font("Consolas", Font.PLAIN, 17));
+			guessThis[i].setBounds(i*15, 0, 15, 15);
+			guessThis[i].setVisible(false);
 		}
-		
-		JButton tes = new JButton("tes");
-		tes.setFont(new Font("Consolas", Font.PLAIN, 11));
-		panel.add(tes);
-		panel.add(labelHuruf);
+
+		JLabel hide[] = new JLabel[guessThisString.length()];
+		for (int i = 0; i < guessThisString.length(); i++) {
+			hide[i] = new JLabel("-");
+			pointer.add(hide[i]);
+			hide[i].setForeground(Color.GRAY);
+			hide[i].setFont(new Font("Consolas", Font.PLAIN, 17));
+			hide[i].setBounds(i*15, 0, 15, 15);
+		}
+
+		alphabet = new JButton[26];
+		for (int i = 'A'; i <= 'Z'; i++) {
+			char b = (char) i;
+			alphabet[i - 'A'] = new JButton("" + b);
+			alphabet[i - 'A'].setFont(new Font("Consolas", Font.PLAIN, 14));
+			button.add(alphabet[i - 'A']);
+			alphabet[i - 'A'].addActionListener(t);
+		}
 	}
-	
+
+	public void changePicture(int a) {
+		String res = "/res/" + (10 - a) + ".png";
+		hangman.setIcon(new ImageIcon(GuiTugas2.class.getResource(res)));
+		hangman.setText("");
+	}
+
+	public void setMessage(String s) {
+		messageLabel.setText(s);
+	}
 }
